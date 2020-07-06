1b609ebb9fe979a8885e65aef73aa9616ec9e3f7
ovaleanujnpr <32875442+ovaleanujnpr@users.noreply.github.com>
2020-07-06 10:53:41 +0100

    fourth

--- centos/centos_prep_install_k8s_containerd.sh
+++ centos/centos_prep_install_k8s_containerd.sh
@@ -15,7 +15,7 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum update -y && yum install -y containerd.io
 mkdir -p /etc/containerd
 containerd config default > /etc/containerd/config.toml
-systemctl restart containerd
+systemctl restart containerd && systemctl enable containerd
 
 bash -c 'cat <<EOF > /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1

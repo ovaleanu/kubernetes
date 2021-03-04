2b29ee0854fc7b3a345c6bb183d5930dc106b01e
ovaleanu <ovaleanu@juniper.net>
2021-03-04 18:21:50 +0000

    k8s version

--- ubuntu/ubuntu_prep_install_k8s_containerd.sh
+++ ubuntu/ubuntu_prep_install_k8s_containerd.sh
@@ -32,7 +32,7 @@ deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF'
 
 apt-get update
-apt-get install -y kubelet=1.19.7-00 kubeadm=1.19.7-00 kubectl=1.19.7-00
+apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
 echo "runtime-endpoint: unix:///run/containerd/containerd.sock" > /etc/crictl.yaml

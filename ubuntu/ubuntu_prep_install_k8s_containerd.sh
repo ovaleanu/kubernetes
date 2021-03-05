d1b627da5a16394ec2963707b32ffdb5eaa30095
ovaleanu <ovaleanu@juniper.net>
2021-03-05 14:33:43 +0000

    change containerd version

--- ubuntu/ubuntu_prep_install_k8s_containerd.sh
+++ ubuntu/ubuntu_prep_install_k8s_containerd.sh
@@ -20,7 +20,14 @@ EOF'
 
 sysctl --system
 
-apt-get update && apt-get install -y containerd
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+apt-get update && apt-get install -y containerd.io
 mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 systemctl restart containerd
--- ubuntu/ubuntu_prep_install_k8s_docker.sh
+++ ubuntu/ubuntu_prep_install_k8s_docker.sh
@@ -9,7 +9,7 @@ net.bridge.bridge-nf-call-iptables = 1
 EOF'
 sysctl --system
 
-apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 add-apt-repository \
@@ -30,7 +30,7 @@ bash -c 'cat <<EOF > /etc/docker/daemon.json
 EOF'
 
 apt-get update && apt-get install -y \
-  containerd.io=1.2.13-2 \
+  containerd.io \
   docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) \
   docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
 
